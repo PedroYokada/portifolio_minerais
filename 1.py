ccec9237d26a9afabb95dd899a74369629f2d82a
PedroYokada <pedro_hlyokada@outlook.com>
2025-09-22 09:06:18 -0300

    Portifolio

---
+++ 1.py
@@ -0,0 +1,13 @@
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+soma = 0
+
+for i in range (len(matriz)):
+    for j in range(len(matriz[i])):
+        soma += matriz[i][j]
+
+print(soma)
---
+++ script.js
@@ -0,0 +1,17 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img");
+
+let idx = 0;
+
+function carrosel() {
+  idx++;
+
+  if (idx > img.length - 1) {
+    idx = 0;
+  }
+
+  // imgs.style.transform = `translateX(${-idx *550}px)`;
+  imgs.style.transform = `translateX(${-idx * img[0].clientWidth}px)`;
+}
+
+setInterval(carrosel, 1800);
