14950a532331bf6da84b8ad8a5ac37c99dbddf1d
PedroYokada <pedro_hlyokada@outlook.com>
2025-11-21 20:57:10 -0300

    Atualização portifólio

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
+// const imgs = document.getElementById("img");
+// const img = document.querySelectorAll("#img img");
 
-let idx = 0;
+// let idx = 0;
 
-function carrosel() {
-  idx++;
+// function carrosel() {
+//   idx++;
 
-  if (idx > img.length - 1) {
-    idx = 0;
-  }
+//   if (idx > img.length - 1) {
+//     idx = 0;
+//   }
 
-  // imgs.style.transform = `translateX(${-idx *550}px)`;
-  imgs.style.transform = `translateX(${-idx * img[0].clientWidth}px)`;
-}
+//   // imgs.style.transform = `translateX(${-idx *550}px)`;
+//   imgs.style.transform = `translateX(${-idx * img[0].clientWidth}px)`;
+// }
 
-setInterval(carrosel, 1800);
+// setInterval(carrosel, 1800);
